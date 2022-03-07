29b662444bbcd1b6134e951afae6a2510c05e45c
luanagoncalvess <luanagoncalves@dcc.ufrj.br>
2022-03-06 23:17:16 -0300

    Round robin

---
+++ functions.h
@@ -0,0 +1,151 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+enum tiposFilas{altaPrioridade, baixaPrioridade, disco, fita, impressora};
+
+typedef struct Process {
+  int pid;
+  int ppid;
+  char* status;
+  int t;
+  int arrivalTime;
+  int burstTime;
+  int priority;
+  int* io;
+  int current_io;
+  int t_io;
+} Process;
+
+typedef struct Queue {
+  Process** queue;
+  int front;
+  int back;
+  int size;
+} Queue;
+
+Process* processCreate(int pid, int ppid, int arrivalTime, int burstTime, int* io) {
+  Process* p = (Process*) malloc(sizeof(Process));
+  p->pid = pid;
+  p->ppid = 0;
+  p->status = "pronto";
+  p->t=0;
+  p->arrivalTime = arrivalTime;
+  p->burstTime = burstTime;
+  p->priority = 0;
+  p->io = (int*) malloc(sizeof(int) * p->burstTime);
+  int i=0;
+  for(i = 0; i < p->burstTime; i++)
+    p->io[i] = io[i];
+  p->io = io;
+  p->current_io = 0;
+  p->t_io = 0;
+  return p;
+}
+
+Queue* queueCreate(Process** procs, int size) {
+  Queue* q = (Queue*) malloc(sizeof(Queue));
+  q->front = 0;
+  q->back = size;
+  q->size = size;
+  q->queue = (Process**) malloc(sizeof(Process*) * 5);
+  if(procs == NULL) return q;
+  int i;
+  for(i = 0; i < size; i++)
+    q->queue[i] = procs[i];
+  return q;
+}
+
+
+Process* removeProcessInQueue(Queue* q) {
+  if(q->size == 0) return NULL;
+  q->size--;
+  Process* p = q->queue[q->front];
+  q->queue[q->front++] = NULL;
+  return p;
+}
+
+bool addProcessInQueue(Queue* q, Process* p) {
+  if(q->size == 5-1 || p==NULL) return 0;
+  q->size++;
+  q->back++;
+  q->queue[q->back-1] = p;
+  return 1;
+}
+
+
+
+char* stringQueue(Queue* q) {
+
+  	char* s = (char*) malloc(sizeof(char) * (5)*2);
+	  int s_idx = 0;
+	  int i = q->front;
+	  s[s_idx++] = '[';
+	  s[s_idx++] = ' ';
+	  if(q->size != 0) {
+	    do {
+	      s[s_idx++] = q->queue[i]->pid + '0';
+	      s[s_idx++] = ' ';
+	     do {
+	    		i = (i + 1) % (q->back);
+	  	  } while(q->queue[i] == NULL && i != q->front);
+   
+	    } while(i != q->front);
+	  }
+	  s[s_idx++] = ']';
+	  s[s_idx] = '\0';
+	  	  
+	  return s;
+}
+
+
+void printQueue(Queue** q) {
+	printf("queues: \n\thigh: %s\n\tlow: %s\nio:\n\tdisk: %s\n\tmag_tape: %s\n\tprinter: %s\n\n",
+          stringQueue(q[0]),
+          stringQueue(q[1]),
+          stringQueue(q[2]),
+          stringQueue(q[3]),
+          stringQueue(q[4]));
+}
+
+void executeIo(Queue** queues, Process** process) {
+  int i;
+  for(i = disco; i <= impressora; i++) {
+    Queue* q = queues[i];
+    if(q->size == 0) continue;
+    Process* p = removeProcessInQueue(q);
+    p->t_io--;
+    if(p->t_io == 0) {
+      switch(p->current_io) {
+        case disco:
+          p->priority = 1;
+          addProcessInQueue(queues[1], p);
+          break;
+        case fita:
+        case impressora:
+          p->priority = 0;
+          addProcessInQueue(queues[0], p);
+          break;
+      }
+      p->current_io = 0;
+      continue;
+    }
+    addProcessInQueue(q, p);
+  }
+}
+
+
+bool processLeft(Queue** queues) {
+  int i=0;
+  for(i = altaPrioridade; i <= impressora; i++)
+    if(queues[i]->size > 0) return true;
+  return false;
+}
+
+
+
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+#define TOTAL_PROCESSOS 5
+#define QUANTUM 3
+
+int t=0;
+
+bool executeProcess(Queue** q, Process** po, int queueIndex) {
+   if(queueIndex == -1) {
+    printQueue(q);
+    int i;
+    for(i = 0; i < QUANTUM; i++) {
+      executeIo(q,po);
+      t++;
+    }
+    return true;
+  }
+	
+  printQueue(q);
+  
+  Process* p = removeProcessInQueue(q[queueIndex]);
+  p->status = "executando";
+  bool went_io = false;
+  if(p->priority < 1) p->priority++;
+  int i;
+  for(i = 0; i < QUANTUM; i++) {
+    if(p->current_io != 0) {
+      executeIo(q,po);
+      continue;
+    }
+    // check if io op now
+    if(p->io[p->t] != -1 && p->t < p->burstTime) {
+      p->status = "aguardando";
+      p->t_io = 4;
+      p->current_io = p->io[p->t];
+      addProcessInQueue(q[p->io[p->t]], p);
+      went_io = true;
+      p->t++;
+    }
+
+    if(p->t + 1 <= p->burstTime && p->current_io == 0) p->t++;
+
+    executeIo(q,po);
+    t++;
+  }
+  printf("scheduled proc: %5d, service time left: %3d\n\n",
+         p->pid, p->burstTime - p->t);
+
+  // if done executing
+  if(p->t == p->burstTime) return true;
+
+  if(!went_io && p->current_io == 0 && !addProcessInQueue(q[p->priority], p)) {
+    // should't happen
+    printf("error: can't push to queue %d!\n", queueIndex);
+    return false;
+  }
+  if(p->current_io == 0) p->status = "pronto";
+  return true;
+  
+}
+
+
+
+
+int main(){
+
+    //criação dos processos
+    Process* processos[TOTAL_PROCESSOS];
+    int i=0;
+    for(i=0; i<TOTAL_PROCESSOS; i++){
+    	//int pid, int ppid, int arrivalTime, int burstTime, int* io
+        processos[i] = processCreate(i, 0, 0, 4, 0);
+    }
+
+    //criação dos tipos de filas
+    enum tiposFilas{altaPrioridade, baixaPrioridade, disco, fita, impressora};
+    
+    Queue* filas[5];
+
+    for(i=altaPrioridade;i<=impressora;i++){
+    	// Process** procs, int size
+        filas[i] = queueCreate(NULL,0);
+    }
+    
+    //Simulation - scheduler
+    do{
+	
+	    bool executed = false;
+	    
+	    for(i = 0; i < TOTAL_PROCESSOS; i++){
+	    	if(processos[i]->arrivalTime > t-QUANTUM && processos[i]->arrivalTime <= t){
+	    		bool a = addProcessInQueue(filas[processos[i]->priority], processos[i]);
+			}
+		}
+		
+		
+		//ainda não avaliado
+		for(i=altaPrioridade;i<=impressora;i++){
+	      if(filas[i]->size == 0) continue;
+	      executed = executeProcess(filas,processos,i);
+		    break;
+	    }
+	    if(executed == false) {
+	      executed = executeProcess(filas,processos,-1);
+	    }
+	    
+	}while(processLeft(filas));
+    
+    return 0;
+}
