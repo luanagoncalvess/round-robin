aa4c994d25554bc0d408c121c21a24594f1f1495
Carla <carlamoreno08@gmail.com>
2022-03-06 23:53:22 -0300

    adição de comentários e pequenas mudanças

--- functions.h
+++ functions.h
@@ -3,26 +3,29 @@
 
 enum tiposFilas{altaPrioridade, baixaPrioridade, disco, fita, impressora};
 
+// Struct que simula um processo
 typedef struct Process {
   int pid;
   int ppid;
   char* status;
   int t;
-  int arrivalTime;
-  int burstTime;
+  int arrivalTime; // Instante de chegada do processo (ou instante de ativação)
+  int burstTime; // Tempo de serviço do processo
   int priority;
-  int* io;
-  int current_io;
-  int t_io;
+  int* io; // Vetor de tamanho <burst_time> que armazena o tipo de I/O do processo no index correspondente ao instante de tempo em que a operação é excutada
+  int current_io; // Tipo de operação I/O atual (valor é 0 caso não haja tipo)
+  int t_io; // Tempo restante para a operação I/O atual
 } Process;
 
+// Fila (FIFO) dos ponteiros de <Process>
 typedef struct Queue {
-  Process** queue;
-  int front;
-  int back;
-  int size;
+  Process** queue; // Vetor de ponteiros de <Process>, de tamanho <size>
+  int front; // Index do primeiro elemento da fila
+  int back; // Index do último elemento da fila
+  int size; // Número de elementos na fila
 } Queue;
 
+// Cria um novo processo com argumentos como campos. Gera como saída um ponteiro para o <Process> criado
 Process* processCreate(int pid, int ppid, int arrivalTime, int burstTime, int* io) {
   Process* p = (Process*) malloc(sizeof(Process));
   p->pid = pid;
@@ -38,6 +41,7 @@ Process* processCreate(int pid, int ppid, int arrivalTime, int burstTime, int* i
   return p;
 }
 
+// Cria uma nova fila com argumentos como campos. Gera como saída uma fila
 Queue* queueCreate(Process** procs, int size) {
   Queue* q = (Queue*) malloc(sizeof(Queue));
   q->front = 0;
@@ -51,15 +55,7 @@ Queue* queueCreate(Process** procs, int size) {
   return q;
 }
 
-
-Process* removeProcessInQueue(Queue* q) {
-  if(q->size == 0) return NULL;
-  q->size--;
-  Process* p = q->queue[q->front];
-  q->queue[q->front++] = NULL;
-  return p;
-}
-
+// Adiciona processo à fila
 bool addProcessInQueue(Queue* q, Process* p) {
   if(q->size == 5-1 || p==NULL) return 0;
   q->size++;
@@ -68,10 +64,17 @@ bool addProcessInQueue(Queue* q, Process* p) {
   return 1;
 }
 
+// Remove processo da fila
+Process* removeProcessInQueue(Queue* q) {
+  if(q->size == 0) return NULL;
+  q->size--;
+  Process* p = q->queue[q->front];
+  q->queue[q->front++] = NULL;
+  return p;
+}
 
-
+// Retorna uma fila como string
 char* stringQueue(Queue* q) {
-
   	char* s = (char*) malloc(sizeof(char) * (5)*2);
 	  int s_idx = 0;
 	  int i = q->front;
@@ -84,18 +87,17 @@ char* stringQueue(Queue* q) {
 	     do {
 	    		i = (i + 1) % (q->back);
 	  	  } while(q->queue[i] == NULL && i != q->front);
-   
+
 	    } while(i != q->front);
 	  }
 	  s[s_idx++] = ']';
 	  s[s_idx] = '\0';
-	  	  
 	  return s;
 }
 
-
+// Imprime a fila
 void printQueue(Queue** q) {
-	printf("queues: \n\thigh: %s\n\tlow: %s\nio:\n\tdisk: %s\n\tmag_tape: %s\n\tprinter: %s\n\n",
+	printf("Filas: \n\talta prioridade: %s\n\tbaixa prioridade: %s\nI/O:\n\tdisco: %s\n\tfita mag.: %s\n\timpressora: %s\n\n",
           stringQueue(q[0]),
           stringQueue(q[1]),
           stringQueue(q[2]),
@@ -103,6 +105,7 @@ void printQueue(Queue** q) {
           stringQueue(q[4]));
 }
 
+// Executa a operação I/O em uma unidade de tempo
 void executeIo(Queue** queues, Process** process) {
   int i;
   for(i = disco; i <= impressora; i++) {
@@ -129,19 +132,10 @@ void executeIo(Queue** queues, Process** process) {
   }
 }
 
-
+// Verfifca se ainda existem processos a serem rodados
 bool processLeft(Queue** queues) {
   int i=0;
   for(i = altaPrioridade; i <= impressora; i++)
     if(queues[i]->size > 0) return true;
   return false;
 }
-
-
-
-
-
-
-
-
-
--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
+
 #define TOTAL_PROCESSOS 5
 #define QUANTUM 3
 
 int t=0;
 
+// Executa o prÃ³ximo processo dado um queueIndex. Retorna "true" caso seja executado e "false" caso haja erro
 bool executeProcess(Queue** q, Process** po, int queueIndex) {
    if(queueIndex == -1) {
     printQueue(q);
@@ -16,9 +18,9 @@ bool executeProcess(Queue** q, Process** po, int queueIndex) {
     }
     return true;
   }
-	
+
   printQueue(q);
-  
+
   Process* p = removeProcessInQueue(q[queueIndex]);
   p->status = "executando";
   bool went_io = false;
@@ -29,7 +31,7 @@ bool executeProcess(Queue** q, Process** po, int queueIndex) {
       executeIo(q,po);
       continue;
     }
-    // check if io op now
+    // Checa se existe operaÃ§Ã£o de I/O
     if(p->io[p->t] != -1 && p->t < p->burstTime) {
       p->status = "aguardando";
       p->t_io = 4;
@@ -44,20 +46,19 @@ bool executeProcess(Queue** q, Process** po, int queueIndex) {
     executeIo(q,po);
     t++;
   }
-  printf("scheduled proc: %5d, service time left: %3d\n\n",
-         p->pid, p->burstTime - p->t);
+  printf("Tempo de serviÃ§o restante: %3d\n\n",p->burstTime - p->t);
 
-  // if done executing
+  // Caso termine de executar
   if(p->t == p->burstTime) return true;
 
   if(!went_io && p->current_io == 0 && !addProcessInQueue(q[p->priority], p)) {
-    // should't happen
-    printf("error: can't push to queue %d!\n", queueIndex);
+    // Caso um erro seja encontrado
+    printf("Erro: nÃ£o Ã© possÃ­vel adicionar Ã  fila %d!\n", queueIndex);
     return false;
   }
   if(p->current_io == 0) p->status = "pronto";
   return true;
-  
+
 }
 
 
@@ -65,7 +66,7 @@ bool executeProcess(Queue** q, Process** po, int queueIndex) {
 
 int main(){
 
-    //criação dos processos
+    //criaï¿½ï¿½o dos processos
     Process* processos[TOTAL_PROCESSOS];
     int i=0;
     for(i=0; i<TOTAL_PROCESSOS; i++){
@@ -73,29 +74,29 @@ int main(){
         processos[i] = processCreate(i, 0, 0, 4, 0);
     }
 
-    //criação dos tipos de filas
+    //criaï¿½ï¿½o dos tipos de filas
     enum tiposFilas{altaPrioridade, baixaPrioridade, disco, fita, impressora};
-    
+
     Queue* filas[5];
 
     for(i=altaPrioridade;i<=impressora;i++){
     	// Process** procs, int size
         filas[i] = queueCreate(NULL,0);
     }
-    
+
     //Simulation - scheduler
     do{
-	
+
 	    bool executed = false;
-	    
+
 	    for(i = 0; i < TOTAL_PROCESSOS; i++){
 	    	if(processos[i]->arrivalTime > t-QUANTUM && processos[i]->arrivalTime <= t){
 	    		bool a = addProcessInQueue(filas[processos[i]->priority], processos[i]);
 			}
 		}
-		
-		
-		//ainda não avaliado
+
+
+		//ainda nï¿½o avaliado
 		for(i=altaPrioridade;i<=impressora;i++){
 	      if(filas[i]->size == 0) continue;
 	      executed = executeProcess(filas,processos,i);
@@ -104,8 +105,8 @@ int main(){
 	    if(executed == false) {
 	      executed = executeProcess(filas,processos,-1);
 	    }
-	    
+
 	}while(processLeft(filas));
-    
+
     return 0;
 }
